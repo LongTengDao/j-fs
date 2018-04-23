def503491d428268a134567f75ab968d74b6d8ba
龙腾道 <LongTengDao@LongTengDao.com>
2018-04-24 00:53:33 +0800

    实现了对 createReadStream、createWriteStream、asar 的支持

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 原生文件系统模块的 `promisify` 扩展。
 
-对于 `require('fs')` 模块的每一个有 `*Sync` 对应的 `*` 方法，都生成一个 `promisify` 版本的 `*Async` 方法。
+对于 `require('fs')` 模块的每一个有 `fs.xxxSync` 对应的 `fs.xxx` 方法，都生成一个 `promisify` 版本的 `fs.xxxAsync` 方法。
 
-对于废弃的 `fs.exists` 方法，其 `promisify` 版本是基于 `fs.access` 方法实现的。
+对于废弃的 `fs.exists` 方法，其 `promisify` 版本 `fs.existsAsync` 是基于 `fs.access` 方法实现的。
+
+另外 `fs.createReadStream` 方法的 `promisify` 版本 `fs.createAsyncReadStream`，是在原始返回对象上增加了异步遍历器接口，和 `readAsync` 方法。
+
+而 `fs.createWriteStream` 方法的 `promisify` 版本 `fs.createAsyncWriteStream`，是在原始返回对象上增加了 `writeAsync` 方法。
+
+在 `electron` 环境中，本模块是基于 `require('original-fs')` 实现的（等价于原始 `Node.js` 环境下的 `require('fs')` 模块），并增加了一个属性 `asar`，它的值是以与本模块同样方式扩展的 `require('fs')` 模块（额外支持了将路径中的 `.asar` 文件作为文件夹访问的能力）。
+
+其它情况下，本模块是直接基于 `require('fs')` 实现扩展的。
---
+++ dist/lib/@.js
@@ -0,0 +1,150 @@
+'use strict';
+
+const { asyncIterator } = Symbol;
+
+(({ Async, ExistsAsync, CreateAsyncReadStream, CreateAsyncWriteStream })=>{
+
+	try{
+		require('electron');
+		module.exports = Fs( 'original-fs' );
+		module.exports.asar = Fs( 'fs' );
+	}
+	catch(error){
+		module.exports = Fs( 'fs' );
+	}
+
+	function Fs( name ){
+		const fs = require( name );
+		const exports = Object.create(null);
+		for( const name in fs ){
+			if( name+'Sync' in fs ){
+				exports[name+'Async'] = Async( fs[name] );
+			}
+		}
+		exports['existsAsync'] = ExistsAsync( fs );
+		exports['createAsyncReadStream'] = CreateAsyncReadStream( fs );
+		exports['createAsyncWriteStream'] = CreateAsyncWriteStream( fs );
+		return Object.assign( exports, fs );
+	}
+
+})({
+
+	Async:method=>
+		function(){
+			return new Promise((resolve,reject)=>
+				method(...arguments,(error,value)=>
+					error===null ? resolve(value) : reject(error)
+				)
+			);
+		},
+
+	ExistsAsync:({ access })=>
+		function(){
+			return new Promise((resolve,reject)=>
+				access(arguments[0],error=>
+					error===null ? resolve(true) : ( error.code==='ENOENT' ? resolve(false) : reject(error) )
+				)
+			);
+		},
+
+	CreateAsyncReadStream:({ createReadStream })=>
+		function(){
+			const stream = createReadStream(...arguments).on('error',onError).on('readable',onReadable);
+			stream[asyncIterator] = {
+				stream,
+				resolve:undefined,
+				reject:undefined,
+				error:null,
+				readable:false,
+				done:false,
+				next,
+			};
+			stream.readAsync = readAsync;
+			return stream;
+		},
+
+	CreateAsyncWritenStream:({ createWriteStream })=>
+		function(){
+			const stream = createWriteStream(...arguments).on('error',onError).on('drain',onDrain);
+			stream[asyncIterator] = {
+				stream,
+				resolve:undefined,
+				reject:undefined,
+				error:null,
+			};
+			stream.writeAsync = writeAsync;
+			return stream;
+		},
+
+});
+
+async function readAsync(){
+	return ( await this[asyncIterator].next() ).value || null;
+}
+
+function writeAsync(){
+	return new Promise((resolve,reject)=>{
+		const { error } = this[asyncIterator];
+		if( error ){ return reject( error ); }
+		if( this.write(...arguments) ){ return resolve(); }
+		this[asyncIterator].resolve = resolve;
+		this[asyncIterator].reject = reject;
+	});
+}
+
+function onError(error){
+	this.removeAllListeners();
+	this[asyncIterator].error = error;
+	const { reject } = this[asyncIterator];
+	if( reject ){
+		this[asyncIterator].reject = undefined;
+		this[asyncIterator].resolve = undefined;
+		return reject( error );
+	}
+}
+
+function onDrain(){
+	const { resolve } = this[asyncIterator];
+	this[asyncIterator].reject = undefined;
+	this[asyncIterator].resolve = undefined;
+	return resolve();
+}
+
+function onReadable(){
+	const { resolve } = this[asyncIterator];
+	if( resolve ){
+		this[asyncIterator].reject = undefined;
+		this[asyncIterator].resolve = undefined;
+		const chunk = this.read();
+		if( chunk===null ){
+			this.removeAllListeners();
+			return resolve({ done:this[asyncIterator].done=true, value:undefined });
+		}
+		else{
+			return resolve({ done:false, value:chunk });
+		}
+	}
+	else{
+		this[asyncIterator].readable = true;
+	}
+}
+
+function next(){
+	return new Promise((resolve,reject)=>{
+		if( this.readable ){
+			this.readable = false;
+			const chunk = this.stream.read();
+			if( chunk===null ){
+				this.stream.removeAllListeners();
+				return resolve({ done:this.done=true, value:undefined });
+			}
+			else{
+				return resolve({ done:false, value:chunk });
+			}
+		}
+		if( this.done ){ return resolve({ done:true }); }
+		if( this.error ){ return reject( this.error ); }
+		this.resolve = resolve;
+		this.reject = reject;
+	});
+}
